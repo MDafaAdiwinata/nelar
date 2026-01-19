d500f55e938ed0a591e18fe6d60358c470fa40fb
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-01-19 14:10:17 +0700

    ProductController - Store Data Function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,12 +8,37 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $products = Product::latest()->get();
         return Inertia::render('Admin/Products/Index', compact('products'));
     }
 
-    public function create() {
+    public function create()
+    {
         return Inertia::render('Admin/Products/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'price' => 'numeric|required',
+            'description' => 'required|string',
+            'image' => 'required|mimes:png,jpg,jpeg|max:2048',
+        ]);
+
+        $data = $request->only([
+            'name',
+            'price',
+            'description',
+        ]);
+
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('products', 'public');
+            $data['image'] = $imagePath;
+        }
+        Product::create($data);
+        return redirect()->route('products.index')->with('success', 'Product Created Successfully');
     }
 }
